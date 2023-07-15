0ed639643e02c6c98af067f56d63344b724bfdcd
Ponomarev Denis <desa-196@yandex.ru>
2023-07-15 12:42:31 +0300

    перенес задачу 1 в отдельный метод

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args){
+        task1();
+
+    }
+    public static void task1(){
         Cat cat = new Cat("Барсик", 5);
         Owner owner = new Owner("Николай");
         cat.setOwner(owner);
         cat.greet();
-
     }
 }
