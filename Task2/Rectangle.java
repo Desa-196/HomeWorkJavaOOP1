18d92dcf7aa34568b75e33ccef76220121676977
Ponomarev Denis <desa-196@yandex.ru>
2023-07-15 12:53:45 +0300

    Добавил поля ширины и высоты, геттеры и сеттеры в классе Rectangle

---
+++ Task2/Rectangle.java
@@ -0,0 +1,19 @@
+package Task2;
+
+public class Rectangle {
+    private double height;
+    public double getHeight(){
+        return height;
+    }
+    public void setHeight(double height){
+        this.height = height;
+    }
+
+    private double width;
+    public double getWidth(){
+        return width;
+    }
+    public void setWidtht(double width){
+        this.width = width;
+    }
+}
