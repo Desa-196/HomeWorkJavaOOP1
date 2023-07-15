32d853f097c7aceedee2d25488ce9064963bae0a
Ponomarev Denis <desa-196@yandex.ru>
2023-07-15 14:00:27 +0300

    Создал новую папку и файл Main.java по 3 задаче

--- Task2/Main.java
+++ Task2/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Rectangle rectangle1 = new Rectangle();
         System.out.println("Прямоугольник 1");
         System.out.printf("Ширина = %.1f Высота = %.1f \n", rectangle1.getWidth(), rectangle1.getHeight());
-        System.out.printf("Площадь = %.1f Периметр = %.1f \n", rectangle1.calculateArea(), rectangle1.calculatePerimeter());
+        System.out.printf("Площадь = %.1f Периметр = %.1f \n\n", rectangle1.calculateArea(), rectangle1.calculatePerimeter());
 
         //Передача в контруктор высоты и ширины
         Rectangle rectangle2 = new Rectangle(15.5, 12);
