820c802b0d0e2528226ad14710813a5f6c1a91c7
Ponomarev Denis <desa-196@yandex.ru>
2023-07-15 12:25:33 +0300

    Добавил конструкторы в классы Cat и Owner

--- Cat.java
+++ Cat.java
@@ -1,3 +1,33 @@
 class Cat{
-    
+    private Owner owner;
+
+    public Owner getOwner(){
+        return owner;
+    }
+    public void setOwner(Owner owner){
+        this.owner = owner;
+    }
+
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    private int age;
+    public int getAge(){
+        return age;
+    }
+    public void setName(int age){
+        this.age = age;
+    }
+    public Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public void greet(){
+        System.out.printf("Мяу! Меня зовут %s. Мне %d года(лет). Мой владелец - %s", name, age, owner.getName());
+    }
 }
